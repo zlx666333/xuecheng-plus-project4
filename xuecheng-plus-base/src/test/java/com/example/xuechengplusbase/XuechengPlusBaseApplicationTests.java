b937d4aa5ffc4455f26baddf42543b9007c1c18b
zhaolixin <2536338590@qq.com>
2023-01-23 00:40:09 +0800

    Initial commit

---
+++ xuecheng-plus-base/src/test/java/com/example/xuechengplusbase/XuechengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xuechengplusbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
