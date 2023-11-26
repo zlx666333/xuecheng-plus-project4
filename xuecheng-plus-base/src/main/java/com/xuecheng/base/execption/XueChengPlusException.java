441822cf12094e477a54d52b08516e0276a2c845
zhaolixin <2536338590@qq.com>
2023-11-26 18:11:05 +0800

    重命名异常类

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/execption/XueChengPlusException.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/execption/XueChengPlusException.java
@@ -2,16 +2,16 @@ package com.xuecheng.base.execption;
 /*
 * 统一异常类
 * */
-public class XuechengPlusException extends RuntimeException {
+public class XueChengPlusException extends RuntimeException {
     private static final long serialVersionUID = 5565760508056698922L;
 
     private String errMessage;
 
-    public XuechengPlusException() {
+    public XueChengPlusException() {
         super();
     }
 
-    public XuechengPlusException(String errMessgae) {
+    public XueChengPlusException(String errMessgae) {
         super(errMessgae);
         this.errMessage = errMessgae;
     }
@@ -21,11 +21,11 @@ public class XuechengPlusException extends RuntimeException {
     }
 
     public static void cast(CommonError commonError) {
-        throw new XuechengPlusException(commonError.getErrMessage());
+        throw new XueChengPlusException(commonError.getErrMessage());
     }
 
     public static void cast(String errMessage){
-        throw new XuechengPlusException(errMessage);
+        throw new XueChengPlusException(errMessage);
     }
 
 }
